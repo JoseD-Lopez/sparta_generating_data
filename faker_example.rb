3175325e8b484ef0b9f13ea30c144367d42a2afc
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-04-15 20:37:37 +0100

    Wrote up the readme

---
+++ faker_example.rb
@@ -0,0 +1,4 @@
+require 'faker'
+
+puts "My name is #{Faker::Dog.name} I work at #{Faker::Space.agency_abv} I am not just a tipical #{Faker::Dog.sound} the world I live in has many mountains its called #{Faker::Space.galaxy} the sport around here is called #{Faker::Pokemon.name} yes pokemon move
+I here it is a sport that get played all around the Universe. In my gallaxy it is a big deal I went too #{Faker::Educator.university} to be the best player. The new girl in our Pokemon team #{Faker::RuPaul.queen} is a very good player though she says things like #{Faker::Hobbit.quote} she."
