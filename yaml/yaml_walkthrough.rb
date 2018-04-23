aa1504436de9ef1c23effe4c00b0e61395bba1a3
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-04-23 11:49:04 +0100

    yaml intro

---
+++ yaml/yaml_walkthrough.rb
@@ -0,0 +1,27 @@
+require 'yaml'
+
+class YamlExample
+
+  def initialize
+    @user_credentails = YAML.load_file('user_credentials.yml')
+    @env = YAML.load_file('environments.yml')
+    @nested = YAML.load_file('nested.yml')
+  end
+
+  def print_user_creds
+    puts @user_credentails
+  end
+
+  def print_environments
+    puts @env
+  end
+
+  def print_list_details
+    p @nested
+  end
+end
+
+yaml = YamlExample.new
+yaml.print_user_creds
+yaml.print_environments
+yaml.print_list_details
